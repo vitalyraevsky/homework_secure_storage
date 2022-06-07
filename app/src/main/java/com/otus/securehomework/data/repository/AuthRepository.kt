09763a634e53288b8cb25e5a34d445a1006c6c66
Necris174 <plotnicovvyu@gmail.com>
2022-06-07 12:43:35 +0500

    Task 1

--- app/src/main/java/com/otus/securehomework/data/repository/AuthRepository.kt
+++ app/src/main/java/com/otus/securehomework/data/repository/AuthRepository.kt
@@ -2,14 +2,17 @@ package com.otus.securehomework.data.repository
 
 import com.otus.securehomework.data.Response
 import com.otus.securehomework.data.dto.LoginResponse
+import com.otus.securehomework.data.source.crypto.Security
 import com.otus.securehomework.data.source.local.UserPreferences
 import com.otus.securehomework.data.source.network.AuthApi
+import kotlinx.coroutines.flow.first
 import javax.inject.Inject
 
 class AuthRepository
 @Inject constructor(
     private val api: AuthApi,
-    private val preferences: UserPreferences
+    private val preferences: UserPreferences,
+    private val security: Security
 ) : BaseRepository(api) {
 
     suspend fun login(
@@ -17,6 +20,22 @@ class AuthRepository
         password: String
     ): Response<LoginResponse> {
         return safeApiCall { api.login(email, password) }
+    }
+
+    suspend fun getDecryptedAccessToken(): String? {
+        val accessToken = preferences.accessToken.first()
+        return accessToken?.let { security.decryptAes(it) }
+    }
+
+    suspend fun getDecryptedRefreshToken(): String? {
+        val refreshToken = preferences.refreshToken.first()
+        return refreshToken?.let { security.decryptAes(it) }
+    }
+
+    suspend fun saveEncryptedAccessTokens(accessToken: String, refreshToken: String) {
+        val encryptedAccessToken = security.encryptAes(accessToken)
+        val encryptedRefreshToken = security.encryptAes(refreshToken)
+        preferences.saveAccessTokens(encryptedAccessToken, encryptedRefreshToken)
     }
 
     suspend fun saveAccessTokens(accessToken: String, refreshToken: String) {
