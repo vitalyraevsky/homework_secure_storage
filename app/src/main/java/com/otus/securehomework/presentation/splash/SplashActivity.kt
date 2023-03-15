28c4a8180e98798c99a44c2454b3996ffea2e01a
Vitaliy Farsiyan <ba1er@mail.ru>
2023-03-15 19:30:09 +0300

    task 1 done

--- app/src/main/java/com/otus/securehomework/presentation/splash/SplashActivity.kt
+++ app/src/main/java/com/otus/securehomework/presentation/splash/SplashActivity.kt
@@ -4,28 +4,36 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.asLiveData
+import androidx.lifecycle.lifecycleScope
 import com.otus.securehomework.R
+import com.otus.securehomework.data.repository.AuthRepository
 import com.otus.securehomework.data.source.local.UserPreferences
 import com.otus.securehomework.presentation.auth.AuthActivity
 import com.otus.securehomework.presentation.home.HomeActivity
 import com.otus.securehomework.presentation.startNewActivity
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class SplashActivity  : AppCompatActivity() {
+
+    @Inject
+    lateinit var authRepository: AuthRepository
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
         val userPreferences = UserPreferences(this)
 
-        userPreferences.accessToken.asLiveData().observe(this, Observer {
-            val activity = if (it == null) {
+        lifecycleScope.launch {
+            val accessToken = authRepository.getDecryptedAccessToken()
+            val activity = if (accessToken == null) {
                 AuthActivity::class.java
             } else {
                 HomeActivity::class.java
             }
             startNewActivity(activity)
-        })
+        }
     }
 }
