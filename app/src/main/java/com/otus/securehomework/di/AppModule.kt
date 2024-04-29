d7e7a24feef54a7f98afc3b36dff9e8108df3806
Evtushenko Maksim <meevtushenko@sberbank.ru>
2024-04-29 11:38:47 +0300

    Часть 1 Реализуйте безопасное хранение auth token
     - Реализовал безопасное хранения Auth Token относительно версии ОС
     - Есть две реализации KeyProvider для старых и новых ОС
     - Шифровкой и расшифровкой занимается класс Security

--- app/src/main/java/com/otus/securehomework/di/AppModule.kt
+++ app/src/main/java/com/otus/securehomework/di/AppModule.kt
@@ -1,11 +1,15 @@
 package com.otus.securehomework.di
 
 import android.content.Context
+import android.os.Build
 import com.otus.securehomework.data.repository.AuthRepository
 import com.otus.securehomework.data.repository.UserRepository
 import com.otus.securehomework.data.source.local.UserPreferences
 import com.otus.securehomework.data.source.network.AuthApi
 import com.otus.securehomework.data.source.network.UserApi
+import com.otus.securehomework.domain.secure.KeyProviderImpl
+import com.otus.securehomework.domain.secure.KeyProviderLessThanMImp
+import com.otus.securehomework.domain.secure.Security
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -21,44 +25,46 @@ object AppModule {
     @Provides
     fun provideRemoteDataSource(
         userPreferences: UserPreferences
-    ): RemoteDataSource {
-        return RemoteDataSource(userPreferences)
-    }
+    ): RemoteDataSource = RemoteDataSource(userPreferences)
 
     @Provides
     fun provideAuthApi(
         remoteDataSource: RemoteDataSource,
-    ): AuthApi {
-        return remoteDataSource.buildApi(AuthApi::class.java)
-    }
+    ): AuthApi = remoteDataSource.buildApi(AuthApi::class.java)
 
     @Provides
     fun provideUserApi(
         remoteDataSource: RemoteDataSource,
-    ): UserApi {
-        return remoteDataSource.buildApi(UserApi::class.java)
-    }
+    ): UserApi = remoteDataSource.buildApi(UserApi::class.java)
 
     @Singleton
     @Provides
     fun provideUserPreferences(
-        @ApplicationContext context: Context
-    ): UserPreferences {
-        return UserPreferences(context)
-    }
+        @ApplicationContext context: Context,
+        security: Security
+    ): UserPreferences = UserPreferences(context, security)
 
     @Provides
     fun provideAuthRepository(
         authApi: AuthApi,
         userPreferences: UserPreferences
-    ): AuthRepository {
-        return AuthRepository(authApi, userPreferences)
-    }
+    ): AuthRepository = AuthRepository(authApi, userPreferences)
 
     @Provides
     fun provideUserRepository(
         userApi: UserApi
-    ): UserRepository {
-        return UserRepository(userApi)
+    ): UserRepository = UserRepository(userApi)
+
+    @Provides
+    fun provideSecurity(
+        @ApplicationContext context: Context
+    ): Security {
+        val keyProvider = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            KeyProviderImpl(context)
+        } else {
+            KeyProviderLessThanMImp(context)
+        }
+
+        return Security(keyProvider)
     }
 }
