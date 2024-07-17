b1d427a1d973278504215daeb800a9b116533963
Boris Timofeev <mashin87@gmail.com>
2024-07-17 22:30:59 +0300

    Задание 2

--- app/src/main/java/com/otus/securehomework/presentation/splash/SplashActivity.kt
+++ app/src/main/java/com/otus/securehomework/presentation/splash/SplashActivity.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.asLiveData
+import androidx.lifecycle.lifecycleScope
 import com.otus.securehomework.R
+import com.otus.securehomework.data.security.biometric.BiometricAuthenticator
 import com.otus.securehomework.data.source.local.UserPreferences
 import com.otus.securehomework.presentation.auth.AuthActivity
 import com.otus.securehomework.presentation.home.HomeActivity
 import com.otus.securehomework.presentation.startNewActivity
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -18,17 +21,37 @@ class SplashActivity  : AppCompatActivity() {
     @Inject
     lateinit var userPreferences: UserPreferences
 
+    @Inject
+    lateinit var biometricAuthenticator: BiometricAuthenticator
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        userPreferences.accessToken.asLiveData().observe(this, Observer {
-            val activity = if (it == null) {
-                AuthActivity::class.java
+        userPreferences.accessToken.asLiveData().observe(this) { token ->
+            if (token == null) {
+                showBiometric()
             } else {
-                HomeActivity::class.java
+                navigateToHome()
             }
-            startNewActivity(activity)
-        })
+        }
+    }
+
+    private fun showBiometric() {
+        lifecycleScope.launch {
+            biometricAuthenticator.run(
+                activity = this@SplashActivity,
+                onSuccess = ::navigateToHome,
+                onFailure = ::navigateToAuth,
+            )
+        }
+    }
+
+    private fun navigateToAuth() {
+        startNewActivity(AuthActivity::class.java)
+    }
+
+    private fun navigateToHome() {
+        startNewActivity(HomeActivity::class.java)
     }
 }
