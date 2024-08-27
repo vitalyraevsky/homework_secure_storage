f9ea689bc30e1af68b2ef3792276fb05e1a16f10
mvrlrd <mvrlrd@yandex.ru>
2024-08-27 11:49:32 +0300

    change text color

--- app/src/main/java/com/otus/securehomework/data/crypto/Keys.kt
+++ app/src/main/java/com/otus/securehomework/data/crypto/Keys.kt
@@ -34,162 +34,138 @@ private const val RSA_MODE_LESS_THAN_M = "RSA/ECB/PKCS1Padding" // Режим и
 private const val SHARED_PREFERENCE_NAME = "RSAEncryptedKeysSharedPreferences" // Имя для SharedPreferences, где будут храниться ключи, зашифрованные RSA.
 private const val ENCRYPTED_KEY_NAME = "RSAEncryptedKeysKeyName" // Имя для хранения зашифрованного ключа AES в SharedPreferences.
 
-private const val AES_ALGORITHM = "AES" // Алгоритм шифрования AES.
-private const val AES_KEY_ALIAS = "AES_OTUS_DEMO" // Алиас для AES-ключа в KeyStore.
+private const val AES_ALGORITHM = "AES"
+private const val AES_KEY_ALIAS = "AES_OTUS_DEMO"
 
 class Keys@Inject constructor(
     private val applicationContext: Context
 ) {
 
-    // Ленивая инициализация SharedPreferences для хранения зашифрованных ключей AES.
     private val sharedPreferences by lazy {
         applicationContext.getSharedPreferences(SHARED_PREFERENCE_NAME, Context.MODE_PRIVATE)
     }
-
-    // Ленивая инициализация хранилища ключей KeyStore.
     private val keyStore by lazy {
         KeyStore.getInstance(KEY_PROVIDER).apply {
             load(null)
         }
     }
-
-    // Метод для удаления ключей из KeyStore.
     fun removeKeys(keyAlias: String) {
         keyStore.deleteEntry(keyAlias);
     }
 
-    // Получение (или генерация) AES-ключа.
     fun getAesSecretKey(): SecretKey {
         return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            // Если версия Android >= M, то пытаемся получить ключ из KeyStore или генерируем новый.
-            keyStore.getKey(AES_KEY_ALIAS, null) as? SecretKey ?: generateAesSecretKey()
+          keyStore.getKey(AES_KEY_ALIAS, null) as? SecretKey ?: generateAesSecretKey()
         } else {
-            // Если версия Android < M, получаем ключ из SharedPreferences или генерируем новый.
-            getAesSecretKeyLessThanM() ?: generateAesSecretKey()
+         getAesSecretKeyLessThanM() ?: generateAesSecretKey()
         }
     }
 
-    // Получение AES-ключа для версий Android ниже M из SharedPreferences.
     private fun getAesSecretKeyLessThanM(): SecretKey? {
-        val encryptedKeyBase64Encoded = getSecretKeyFromSharedPreferences() // Получаем зашифрованный ключ в виде строки Base64.
+        val encryptedKeyBase64Encoded = getSecretKeyFromSharedPreferences()
         return encryptedKeyBase64Encoded?.let {
-            val encryptedKey = Base64.decode(encryptedKeyBase64Encoded, Base64.DEFAULT) // Декодируем Base64.
-            val key = rsaDecryptKey(encryptedKey) // Расшифровываем ключ с использованием RSA.
-            SecretKeySpec(key, AES_ALGORITHM) // Создаем и возвращаем SecretKey на основе расшифрованного ключа.
+            val encryptedKey = Base64.decode(encryptedKeyBase64Encoded, Base64.DEFAULT)
+            val key = rsaDecryptKey(encryptedKey)
+            SecretKeySpec(key, AES_ALGORITHM)
         }
     }
 
-    // Расшифровка ключа с использованием RSA.
     private fun rsaDecryptKey(encryptedKey: ByteArray?): ByteArray {
-        val cipher = Cipher.getInstance(RSA_MODE_LESS_THAN_M) // Инициализируем шифр с заданным режимом для RSA.
-        cipher.init(Cipher.DECRYPT_MODE, getRsaPrivateKey()) // Инициализируем шифр для расшифровки с приватным ключом.
-        return cipher.doFinal(encryptedKey) // Возвращаем расшифрованные данные.
+        val cipher = Cipher.getInstance(RSA_MODE_LESS_THAN_M)
+        cipher.init(Cipher.DECRYPT_MODE, getRsaPrivateKey())
+        return cipher.doFinal(encryptedKey)
     }
 
-    // Получение зашифрованного ключа из SharedPreferences.
     private fun getSecretKeyFromSharedPreferences(): String? {
         return sharedPreferences.getString(ENCRYPTED_KEY_NAME, null)
     }
 
 
-    // Метод генерации нового AES-ключа. Если версия Android >= M, используется KeyGenerator.
     private fun generateAesSecretKey(): SecretKey {
         return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            getKeyGenerator().generateKey() // Генерация нового AES-ключа с использованием KeyGenerator.
+            getKeyGenerator().generateKey()
         } else {
-            generateAndSaveAesSecretKeyLessThanM() // Если версия ниже M, генерируем ключ и сохраняем его.
+            generateAndSaveAesSecretKeyLessThanM()
         }
     }
 
-    // Генерация и сохранение AES-ключа для версий Android ниже M.
     private fun generateAndSaveAesSecretKeyLessThanM(): SecretKey {
-        val key = ByteArray(16) // Создание байтового массива длиной 16 байт для ключа.
+        val key = ByteArray(16)
         SecureRandom().run {
-            nextBytes(key) // Генерация случайных байтов для ключа.
+            nextBytes(key)
         }
         val encryptedKeyBase64encoded = Base64.encodeToString(
-            rsaEncryptKey(key), // Шифруем ключ с использованием RSA.
+            rsaEncryptKey(key),
             Base64.DEFAULT
         )
-        // Сохраняем зашифрованный ключ в SharedPreferences.
         sharedPreferences.edit().apply {
             putString(ENCRYPTED_KEY_NAME, encryptedKeyBase64encoded)
-            apply() // Применяем изменения.
+            apply()
         }
-        return SecretKeySpec(key, AES_ALGORITHM) // Возвращаем AES-ключ, созданный из массива байт.
+        return SecretKeySpec(key, AES_ALGORITHM)
     }
 
-    // Метод шифрования ключа с использованием RSA.
+
     private fun rsaEncryptKey(secret: ByteArray): ByteArray {
-        val cipher = Cipher.getInstance(RSA_MODE_LESS_THAN_M) // Инициализация шифра с режимом RSA/ECB/PKCS1Padding.
-        cipher.init(Cipher.ENCRYPT_MODE, getRsaPublicKey()) // Инициализация шифра для шифрования с публичным ключом.
-        return cipher.doFinal(secret) // Возвращаем зашифрованный ключ.
+        val cipher = Cipher.getInstance(RSA_MODE_LESS_THAN_M)
+        cipher.init(Cipher.ENCRYPT_MODE, getRsaPublicKey())
+        return cipher.doFinal(secret)
     }
 
-    // Метод получения KeyGenerator для AES на Android >= M.
     @RequiresApi(Build.VERSION_CODES.M)
     private fun getKeyGenerator() = KeyGenerator.getInstance(AES_ALGORITHM, KEY_PROVIDER).apply {
-        init(getKeyGenSpec()) // Инициализация генератора ключей с параметрами.
+        init(getKeyGenSpec())
     }
 
-    // Метод получения спецификации генерации ключа (KeyGenParameterSpec) для Android >= M.
     @RequiresApi(Build.VERSION_CODES.M)
     private fun getKeyGenSpec(): KeyGenParameterSpec {
         return KeyGenParameterSpec.Builder(
-            AES_KEY_ALIAS, // Устанавливаем алиас ключа.
-            KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT // Указываем, что ключ используется для шифрования и дешифрования.
+            AES_KEY_ALIAS,
+            KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT
         )
-            .setBlockModes(KeyProperties.BLOCK_MODE_GCM) // Устанавливаем режим блочного шифрования GCM.
-            .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_NONE) // Устанавливаем отсутствие паддинга.
-            .setUserAuthenticationRequired(false) // Отключаем требование аутентификации пользователя.
-            .setRandomizedEncryptionRequired(false) // Отключаем требование случайной генерации.
-            .setKeySize(KEY_LENGTH) // Устанавливаем размер ключа в битах.
-            .build() // Создаем и возвращаем спецификацию.
+            .setBlockModes(KeyProperties.BLOCK_MODE_GCM)
+            .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_NONE)
+            .setUserAuthenticationRequired(false)
+            .setRandomizedEncryptionRequired(false)
+            .setKeySize(KEY_LENGTH)
+            .build()
     }
 
 
-
-    // Метод получения приватного RSA-ключа.
     private fun getRsaPrivateKey(): PrivateKey {
         return keyStore.getKey(RSA_KEY_ALIAS, null) as? PrivateKey ?: generateRsaSecretKey().private
-        // Возвращаем приватный ключ, если он существует, иначе генерируем новый.
     }
 
-    // Метод получения публичного RSA-ключа.
     private fun getRsaPublicKey(): PublicKey {
         return keyStore.getCertificate(RSA_KEY_ALIAS)?.publicKey ?: generateRsaSecretKey().public
-        // Возвращаем публичный ключ, если он существует, иначе генерируем новый.
     }
 
-    // Метод генерации новой пары RSA-ключей.
     private fun generateRsaSecretKey(): KeyPair {
         val spec = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            // Для Android >= M создаем KeyGenParameterSpec.
             KeyGenParameterSpec.Builder(
-                RSA_KEY_ALIAS, // Устанавливаем алиас ключа.
-                KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT // Указываем, что ключ используется для шифрования и дешифрования.
+                RSA_KEY_ALIAS,
+                KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT
             )
-                .setBlockModes(KeyProperties.BLOCK_MODE_ECB) // Устанавливаем режим блочного шифрования ECB.
-                .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_RSA_PKCS1) // Устанавливаем паддинг PKCS1.
-                .setUserAuthenticationRequired(true) // Включаем требование аутентификации пользователя.
-                .setRandomizedEncryptionRequired(false) // Отключаем требование случайной генерации.
+                .setBlockModes(KeyProperties.BLOCK_MODE_ECB)
+                .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_RSA_PKCS1)
+                .setUserAuthenticationRequired(true)
+                .setRandomizedEncryptionRequired(false)
                 .build()
         } else {
-            // Для Android < M создаем KeyPairGeneratorSpec.
-            val start: Calendar = Calendar.getInstance() // Начальная дата.
-            val end: Calendar = Calendar.getInstance() // Конечная дата.
-            end.add(Calendar.YEAR, 30) // Устанавливаем срок действия ключа на 30 лет.
+            val start: Calendar = Calendar.getInstance()
+            val end: Calendar = Calendar.getInstance()
+            end.add(Calendar.YEAR, 30)
             KeyPairGeneratorSpec.Builder(applicationContext)
-                .setAlias(RSA_KEY_ALIAS) // Устанавливаем алиас ключа.
-                .setSubject(X500Principal("CN=$RSA_KEY_ALIAS")) // Устанавливаем X500Principal для сертификата.
-                .setSerialNumber(BigInteger.TEN) // Устанавливаем серийный номер.
-                .setStartDate(start.getTime()) // Устанавливаем начальную дату.
-                .setEndDate(end.getTime()) // Устанавливаем конечную дату.
+                .setAlias(RSA_KEY_ALIAS)
+                .setSubject(X500Principal("CN=$RSA_KEY_ALIAS"))
+                .setSerialNumber(BigInteger.TEN)
+                .setStartDate(start.getTime())
+                .setEndDate(end.getTime())
                 .build()
         }
         return KeyPairGenerator.getInstance(RSA_ALGORITHM, KEY_PROVIDER).run {
-            initialize(spec) // Инициализируем генератор ключей с созданной спецификацией.
-            generateKeyPair() // Генерируем и возвращаем пару ключей.
+            initialize(spec)
+            generateKeyPair()
         }
     }
 }
--- app/src/main/java/com/otus/securehomework/data/crypto/Security.kt
+++ app/src/main/java/com/otus/securehomework/data/crypto/Security.kt
@@ -10,55 +10,55 @@ import javax.crypto.Mac
 import javax.crypto.spec.GCMParameterSpec
 import javax.inject.Inject
 
-private const val AES_TRANSFORMATION = "AES/GCM/NoPadding" // Константа, определяющая режим шифрования и заполнения для алгоритма AES.
-private const val IV = "3134003223491201" // Жестко заданный инициализационный вектор (IV), который используется для AES-шифрования.
-private const val GCM_IV_LENGTH = 12 // Длина инициализационного вектора (IV) для режима GCM (12 байт), который используется в AES-шифровании.
+private const val AES_TRANSFORMATION = "AES/GCM/NoPadding"
+private const val IV = "3134003223491201"
+private const val GCM_IV_LENGTH = 12
 
 class Security @Inject constructor() {
-    private val FIXED_IV = IV.toByteArray() // Преобразование строки IV в байтовый массив, который используется как фиксированный инициализационный вектор.
+    private val FIXED_IV = IV.toByteArray()
 
     fun encryptAes(plainText: String, key: Key): String {
-        val cipher = Cipher.getInstance(AES_TRANSFORMATION) // Получаем экземпляр Cipher для алгоритма AES с указанным режимом и заполнением (AES/GCM/NoPadding).
+        val cipher = Cipher.getInstance(AES_TRANSFORMATION)
         cipher.init(
             Cipher.ENCRYPT_MODE,
             key,
             getInitializationVector()
-        ) // Инициализируем шифрование с использованием переданного ключа и вектора инициализации (IV).
-        val encodedBytes = cipher.doFinal(plainText.toByteArray()) // Выполняем шифрование и получаем зашифрованные данные в виде байтового массива.
+        )
+        val encodedBytes = cipher.doFinal(plainText.toByteArray())
         return Base64.encodeToString(
             encodedBytes,
             Base64.NO_WRAP
-        ) // Кодируем зашифрованные данные в строку Base64 и возвращаем результат.
+        )
     }
 
     private fun getInitializationVector(): AlgorithmParameterSpec {
-        val iv = ByteArray(GCM_IV_LENGTH) // Создаем байтовый массив для инициализационного вектора с длиной, заданной GCM_IV_LENGTH.
+        val iv = ByteArray(GCM_IV_LENGTH)
         FIXED_IV.copyInto(
             iv,
             0,
             GCM_IV_LENGTH
-        ) // Копируем фиксированный инициализационный вектор (IV) в созданный массив.
+        )
         return GCMParameterSpec(
             128,
             iv
-        ) // Возвращаем спецификацию параметра для режима GCM с длиной тега аутентификации 128 бит и инициализационным вектором.
+        )
     }
 
     fun decryptAes(encrypted: String, key: Key): String {
-        val cipher = Cipher.getInstance(AES_TRANSFORMATION) // Получаем экземпляр Cipher для алгоритма AES с указанным режимом и заполнением (AES/GCM/NoPadding).
+        val cipher = Cipher.getInstance(AES_TRANSFORMATION)
         cipher.init(
             Cipher.DECRYPT_MODE,
             key,
             getInitializationVector()
-        ) // Инициализируем дешифрование с использованием переданного ключа и вектора инициализации (IV).
+        )
         val decodedBytes = Base64.decode(
             encrypted,
             Base64.NO_WRAP
-        ) // Декодируем зашифрованные данные из строки Base64 в байтовый массив.
-        val decoded = cipher.doFinal(decodedBytes) // Выполняем дешифрование и получаем исходные данные в виде байтового массива.
+        )
+        val decoded = cipher.doFinal(decodedBytes)
         return String(
             decoded,
             Charsets.UTF_8
-        ) // Преобразуем дешифрованные данные в строку и возвращаем результат.
+        )
     }
 }
